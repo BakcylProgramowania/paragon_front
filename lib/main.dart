eee0e01da81d8d4d2e109ec9647a8d8b713c0bc3
Maciej Talarczyk <117164503+muclx@users.noreply.github.com>
2023-12-15 16:03:28 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'pages/signIn.dart';
+import "package:http/http.dart" as http;
 
 void main() {
   runApp(const MyApp());
@@ -13,113 +16,285 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        fontFamily: 'Poppins',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
+// dsad
+// ignore: must_be_immutable
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  String login = "";
+  String password = "";
+  postData() async {
+    var response = http
+        .post(Uri.parse("https://jsonplaceholder.typicode.com/posts"), body: {
+      "id": 1.toString(),
+      "login": login,
+      "password": password,
     });
   }
 
   @override
+  State<MyHomePage> createState() => Login();
+}
+
+class Login extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  var home = MyHomePage();
+  final TextEditingController _textController = TextEditingController();
+  String myValue = '';
+  String holder = "";
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          RichText(
+            text: const TextSpan(
+              style: TextStyle(
+                  fontSize: 80,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.black),
+              children: [
+                TextSpan(
+                    text: 'Para',
+                    style: TextStyle(color: AppColors.primaryColor)),
+                TextSpan(
+                    text: 'gon',
+                    style: TextStyle(color: AppColors.defaultTextColor)),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Container(
+              alignment: Alignment.topLeft,
+              margin: const EdgeInsets.only(top: 40),
+              padding: const EdgeInsets.only(left: 50),
+              child: const Text('Zaloguj sie',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ))),
+          Align(
+            alignment: FractionalOffset.topLeft,
+            child: Container(
+              width: 120,
+              margin: const EdgeInsets.only(left: 50),
+              height: 2,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 35, left: 50, right: 50),
+            child: Form(
+              key: _formKey,
+              child: Column(children: [
+                TextFormField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: const TextStyle(
+                          color: AppColors.grayAccent, fontSize: 15),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide:
+                              const BorderSide(color: AppColors.primaryColor)),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(
+                          color: AppColors.grayAccent,
+                        ),
+                      ),
+                      contentPadding: const EdgeInsets.all(15)),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Hasło",
+                      labelStyle: const TextStyle(
+                          color: AppColors.grayAccent, fontSize: 15),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide:
+                              const BorderSide(color: AppColors.primaryColor)),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(
+                          color: AppColors.grayAccent,
+                        ),
+                      ),
+                      contentPadding: const EdgeInsets.all(15)),
+                  obscureText: true,
+                ),
+                Container(
+                  width: 400,
+                  margin: const EdgeInsets.only(top: 20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primaryColor,
+                      textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700),
+                      padding: const EdgeInsets.all(13),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_textController.text.isNotEmpty) {
+                        myValue = _textController.text;
+                        _textController.text = home.login;
+                      } else {
+                        myValue = 'Pole tekstowe nie może być puste.';
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(myValue),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Zaloguj się',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: InkWell(
+              onTap: () {
+                // Przenoszenie na nową stronę po kliknięciu
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignIn()),
+                );
+              },
+              child: RichText(
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Nie masz konta?',
+                      style: TextStyle(color: AppColors.grayAccent),
+                    ),
+                    TextSpan(
+                      text: ' Zarejestruj się.',
+                      style: TextStyle(
+                        color: AppColors.primaryColor,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 30),
+              width: 350,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 1,
+                      width: 20,
+                      color: AppColors.grayAccent,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      'lub',
+                      style: TextStyle(color: AppColors.grayAccent),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 1,
+                      color: AppColors.grayAccent,
+                    ),
+                  ),
+                ],
+              )),
+          Container(
+              margin: const EdgeInsets.only(top: 30),
+              width: 330,
+              child: Column(
+                children: [
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: AppColors.google,
+                    ),
+                    child: Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/google.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(right: 10),
+                        ),
+                        const Text(
+                          'Zaloguj się z Google',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 16),
+                        )
+                      ],
+                    )),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: AppColors.fb,
+                    ),
+                    child: const Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.facebook, color: Colors.white),
+                        Padding(
+                          padding: EdgeInsets.only(right: 10),
+                        ),
+                        Text(
+                          'Zaloguj się z Facebook',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                              fontSize: 16),
+                        )
+                      ],
+                    )),
+                  ),
+                ],
+              ))
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
