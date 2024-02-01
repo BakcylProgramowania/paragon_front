76911f54ea6894dd8cf0fc9e332e4916474d6e3e
Stanislaw Podrecki <s.podrecki@gmail.com>
2024-02-01 16:18:03 +0100

    added default expansion tile

--- lib/default/default_widgets.dart
+++ lib/default/default_widgets.dart
@@ -47,3 +47,66 @@ class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(70);
 }
+
+
+class DefaultExpansionTile extends StatelessWidget {
+  final String title;
+  final String? imageLink;
+  final List<Widget> children;
+  const DefaultExpansionTile(
+      {super.key, required this.title, required this.children, this.imageLink});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Theme(
+          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 30),
+            child: ExpansionTile(
+                title: Row(children: [
+                  if (imageLink != null)
+                    Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(imageLink!),
+                      ),
+                    ),
+                  Text(
+                    title,
+                  )
+                ]),
+                iconColor: AppColors.primaryColor,
+                collapsedIconColor: AppColors.primaryColor,
+                children: [
+                  ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: children,
+                  )
+                ]),
+          ),
+        ),
+        const SizedBox(
+            width: 330,
+            child: Divider(
+              height: 10,
+              color: Colors.black12,
+            ))
+      ],
+    );
+  }
+}
+
+List<String>? search(
+    {String searchedValue = '', required List<String> friendsList}) {
+  List<String> result = [];
+  for (var element in friendsList) {
+    if (element.toLowerCase().contains(searchedValue.toLowerCase())) {
+      result.add(element);
+    }
+  }
+
+  return result.isNotEmpty ? result : null;
+}
