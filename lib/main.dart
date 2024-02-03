7dc62bea10a4a7cd6291dcd786c526599ff34537
Xingob <xin.roma@gmail.com>
2024-02-03 16:46:32 +0100

    changed bilans & visual look

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double bilans = 30.52;
+
   // History data
   final List<Map<String, dynamic>> historyData = [
     {'icon': Icons.dining, 'name': 'Wspólne jedzenie', 'amount': '51.60 PLN'},
@@ -17,186 +36,160 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Strona główna',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: const Icon(Icons.person),
-            iconSize: 38,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Strona główna',
+          style: TextStyle(
             color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.person),
+          iconSize: 38,
+          color: Colors.black,
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            iconSize: 40,
+            color: const Color(0xFFf85f6a),
             onPressed: () {},
           ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              iconSize: 40,
-              color: const Color(0xFFf85f6a),
-              onPressed: () {},
-            ),
-          ],
-        ),
-
-        //Divider
-        body: Stack(
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+        child: Column(
           children: [
-            Column(
-              children: [
-                const Divider(
-                  color: Colors.grey,
-                ),
-
-                //Some text
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  child: const Text(
-                    'Lorem ipsum dolor sit amet consectetur adipisicing\nelit. Voluptatess quasi iusto officia consequuntur\nquos neque nam magnam ten.Lorem ipsum dolor sit\namet consectetur adipisicing elit. Voluptatess quasi.',
-                    style: TextStyle(
-                        fontSize: 13, color: Color(0xFF656565), fontWeight: FontWeight.bold),
-                  ),
-                ),
-
-                //Big box
-                Container(
-                  width: 280,
-                  height: 160,
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFeeecec),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 5.0,
-                        spreadRadius: 1.0,
-                        offset: Offset(0, 1),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Policzymy twój\nrachunek',
-                        style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(),
-                      const Text(
-                        'Wpisz dane i rozluźnij się.',
-                        style: TextStyle(
-                            fontSize: 14,
-                            color: Color(0xFF656565),
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: 90,
-                        height: 24,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFFf85f6a),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          child: const Text('Policz',
-                              style:
-                                  TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                //Two little boxes
-                const SizedBox(height: 40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: _buildBox('42 PLN', Icons.arrow_upward, Colors.black,
-                          const Color(0xFFf85f6a), const Color(0xFF06e105)),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: _buildBox('30 PLN', Icons.arrow_downward, Colors.black,
-                          const Color(0xFFf85f6a), const Color(0xFFd51616)),
-                    ),
-                  ],
-                ),
-
-                // History
-                const SizedBox(
-                  height: 20,
-                ),
-                Column(
-                  children: historyData.reversed.map((entry) {
-                    return _buildHistoryItem(
-                        entry['icon'], entry['name'], entry['amount']);
-                  }).toList(),
-                ),
-              ],
+            const Divider(
+              color: Colors.grey,
             ),
-
-            // Control Panel
-            Positioned(
-              bottom: 25,
-              left: 40,
-              right: 40,
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFeeecec),
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5.0,
-                      spreadRadius: 1.0,
-                      offset: Offset(0, 1),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildRoundButton(Icons.people, iconSize: 35),
-                    _buildRoundButton(Icons.home, isSelected: true, iconSize: 35),
-                    _buildRoundButton(Icons.history, iconSize: 35),
-                  ],
+            //Some text
+            Container(
+              padding: const EdgeInsets.all(12),
+              child: const Text(
+                'Lorem ipsum dolor sit amet consectetur adipisicing\nelit. Voluptatess quasi iusto officia consequuntur\nquos neque nam magnam ten.Lorem ipsum dolor sit\namet consectetur adipisicing elit. Voluptatess quasi.',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xFF656565),
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
+            //Big box
+            Container(
+              width: 320,
+              height: 160,
+              padding: const EdgeInsets.all(14),
+              decoration: BoxDecoration(
+                color: const Color(0xFFeeecec),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 5.0,
+                    spreadRadius: 1.0,
+                    offset: Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Policzymy twój\nrachunek',
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(),
+                  const Text(
+                    'Wpisz dane i rozluźnij się.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF656565),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: 100,
+                    height: 28,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFf85f6a),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: const Text(
+                        'Policz',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //Bilans
+            const SizedBox(height: 30),
+            _buildBox('Do\n oddania $bilans PLN', Colors.black, const Color(0xFFf85f6a)),
+            // History
+            const SizedBox(
+              height: 25,
+            ),
+            Column(
+              children: historyData.reversed.map((entry) {
+                return _buildHistoryItem(entry['icon'], entry['name'], entry['amount']);
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+      // Control Panel
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        decoration: BoxDecoration(
+          color: const Color(0xFFeeecec),
+          borderRadius: BorderRadius.circular(0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 5.0,
+              spreadRadius: 1.0,
+              offset: Offset(0, 1),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildRoundButton(Icons.people, iconSize: 35),
+            _buildRoundButton(Icons.home, isSelected: true, iconSize: 35),
+            _buildRoundButton(Icons.history, iconSize: 35),
           ],
         ),
       ),
     );
   }
 
-  //Two little boxes WIDGET
-  Widget _buildBox(
-      String text, IconData icon, Color boxColor, Color shadowColor, Color iconColor) {
+  //Bilans WIDGET
+  Widget _buildBox(String text, Color boxColor, Color shadowColor) {
     return Container(
-      width: 120,
-      height: 70,
-      padding: const EdgeInsets.all(12),
+      width: 320,
+      height: 100,
+      padding: const EdgeInsets.all(26),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
             color: shadowColor,
@@ -209,15 +202,57 @@ class MyApp extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(
-            icon,
-            color: iconColor,
-            size: 30,
+          // "Do oddania" text
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Align vertically
+            crossAxisAlignment: CrossAxisAlignment.start, // Align horizontally
+            children: [
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Do',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '\noddania',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Text(
-            text,
-            style: const TextStyle(
-                color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
+          // Amount and PLN
+          Row(
+            children: [
+              // Dynamic amount
+              Text(
+                '$bilans',
+                style: const TextStyle(
+                  color: Color(0xFFf85f6a),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 4), // Spacing between amount and PLN
+              // PLN text
+              const Text(
+                'PLN',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+            ],
           ),
         ],
       ),
@@ -225,15 +260,14 @@ class MyApp extends StatelessWidget {
   }
 
   // History item widget
-  Widget _buildHistoryItem(
-      IconData icon, String name, String amount) {
+  Widget _buildHistoryItem(IconData icon, String name, String amount) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8),
       padding: const EdgeInsets.all(8),
-      width: 300, // Set width to match the parent container
+      width: 320, // Set width to match the parent container
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(20),
         boxShadow: const [
           BoxShadow(
             color: Colors.grey,
@@ -260,8 +294,7 @@ class MyApp extends StatelessWidget {
                 children: [
                   Text(
                     name,
-                    style: const TextStyle(
-                        fontSize: 12, fontWeight: FontWeight.bold),
+                    style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
@@ -269,7 +302,7 @@ class MyApp extends StatelessWidget {
           ),
           Text(
             amount,
-            style: const TextStyle(fontSize: 10, color: Colors.black, fontWeight: FontWeight.bold),
+            style: const TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),
           ),
         ],
       ),
