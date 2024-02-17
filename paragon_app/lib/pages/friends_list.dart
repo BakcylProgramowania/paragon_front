4792dcfa272294400f7334abcbffc85491b297c9
Dominik Lejman <dominiklejman43@gmail.com>
2024-02-17 20:17:50 +0100

    login by correct pass, change in main.dart

---
+++ paragon_app/lib/pages/friends_list.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import '/default/default_widgets.dart';
+
+class FriendsList extends StatefulWidget {
+  final List<String>? friendsList;
+  final Map<String, String>? linksToFriendsPfps;
+  final Map<String, List<String>>? friendsHistory;
+  //we should probably add list with all user names and a map with links to their pfps {user1: linkToPictureForUser1}
+
+  const FriendsList(
+      {super.key,
+      this.friendsList,
+      this.linksToFriendsPfps,
+      this.friendsHistory});
+
+  @override
+  State<FriendsList> createState() => _FriendsListState();
+}
+
+class _FriendsListState extends State<FriendsList> {
+  List<String>? _searchedFriendsList;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.friendsList != null) {
+      _searchedFriendsList = [];
+      for (var element in widget.friendsList!) {
+        _searchedFriendsList!.add(element);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const DefaultAppBar(title: 'Znajomi'),
+        body: (() {
+          if (widget.friendsList != null) {
+            return Column(
+              children: [
+                Container(
+                  margin: const EdgeInsetsDirectional.symmetric(horizontal: 15),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 6,
+                        child: Container(
+                          margin: const EdgeInsets.all(20),
+                          child: DefaultTextField(
+                            onChanged: (searchedValue) {
+                              setState(() {
+                                _searchedFriendsList = search(
+                                    searchingList: widget.friendsList!,
+                                    searchedValue: searchedValue);
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      const Expanded(
+                          flex: 1,
+                          child: Icon(
+                            Icons.qr_code,
+                            size: 35,
+                          )),
+                      const Expanded(
+                          flex: 1, child: Icon(Icons.person_add, size: 35))
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                    width: 330,
+                    child: Divider(height: 10, color: Colors.black12)),
+                () {
+                  if (_searchedFriendsList != null) {
+                    return Expanded(
+                      child: ListView.builder(
+                          //shrinkWrap: true,
+                          itemCount: _searchedFriendsList!.length,
+                          itemBuilder: (context, index) {
+                            return DefaultExpansionTile(
+                              title: _searchedFriendsList![index],
+                              imageLink: widget.linksToFriendsPfps?[
+                                  _searchedFriendsList?[index]],
+                              children: widget.friendsHistory?[
+                                          _searchedFriendsList![index]]
+                                      ?.map((e) => Center(child: Text(e)))
+                                      .toList() ??
+                                  [
+                                    const Center(
+                                        child: Text(
+                                            'Nie dokonałeś jeszcze wspólnych rozliczeń'))
+                                  ],
+                            );
+                          }),
+                    );
+                  } else {
+                    return const Center(
+                        child: Text('Nie można znaleźć szukanych znajomych'));
+                  }
+                }()
+              ],
+            );
+          } else {
+            return const Center(child: Text('Nie masz jeszcze znajomych :('));
+          }
+        })());
+  }
+}
---
+++ paragon_app/lib/pages/sign_in.dart
@@ -0,0 +1,271 @@
+// ignore_for_file: unused_local_variable, avoid_print, file_names
+
+import 'package:flutter/material.dart';
+import '/default/colors.dart';
+import 'dart:convert';
+import "package:http/http.dart" as http;
+import "main_page.dart";
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  late TextEditingController _nameController;
+  late TextEditingController _secondNameCotroller;
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _nameController = TextEditingController();
+    _secondNameCotroller = TextEditingController();
+  }
+
+  // Metoda do wysyłania danych
+  Future<void> fetchData(
+      String name, String email, String password) async {
+    final url = Uri.parse("https://paragon.wroc.ovh" "/register"); // Zaktualizuj URL
+    print("work");
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'username': name,
+          'email': email,
+          'password': password,
+        }),
+      );
+      print(email);
+      print(password);
+      print(name);
+      print("Response status code: ${response.statusCode}");
+      print("Response body: ${response.body}");
+
+      if (response.statusCode == 200 || response.statusCode == 201 ) {
+        print("Sukces: ${response.body}");
+        Navigator.pushReplacement( 
+          context,
+          MaterialPageRoute(builder: (context) => MainPage()),
+          );
+      } else {
+        print("Błąd: ${response.statusCode}");
+        print("Błąd: ${response.body}");
+      }
+    } catch (error) {
+      print("Błąd: $error");
+    }
+  }
+
+  // Metoda do przetwarzania danych
+  void postData() async {
+    if (formKey.currentState!.validate()) {
+      fetchData(
+        _nameController.text,
+        _emailController.text,
+        _passwordController.text,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              title: const Text('Rejestracja'),
+              centerTitle: true,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(
+                  text: const TextSpan(
+                    style: TextStyle(
+                        fontSize: 80,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.black),
+                    children: [
+                      TextSpan(
+                          text: 'Para',
+                          style: TextStyle(color: AppColors.primaryColor)),
+                      TextSpan(
+                          text: 'gon',
+                          style: TextStyle(color: AppColors.defaultTextColor)),
+                    ],
+                  ),
+                ),
+                Container(
+                    alignment: Alignment.topLeft,
+                    margin: const EdgeInsets.only(top: 40),
+                    padding: const EdgeInsets.only(left: 50),
+                    child: const Text('Zarejestruj sie',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ))),
+                Align(
+                  alignment: FractionalOffset.topLeft,
+                  child: Container(
+                    width: 150,
+                    margin: const EdgeInsets.only(left: 50),
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 35, left: 50, right: 50),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                            controller: _nameController,
+                            decoration: InputDecoration(
+                                labelText: "Imie",
+                                labelStyle: const TextStyle(
+                                    color: AppColors.greyAccent, fontSize: 15),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    borderSide: const BorderSide(
+                                        color: AppColors.primaryColor)),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                    color: AppColors.greyAccent,
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.all(15))),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                            controller: _secondNameCotroller,
+                            decoration: InputDecoration(
+                                labelText: "Nazwisko",
+                                labelStyle: const TextStyle(
+                                    color: AppColors.greyAccent, fontSize: 15),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    borderSide: const BorderSide(
+                                        color: AppColors.primaryColor)),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                    color: AppColors.greyAccent,
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.all(15))),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                                labelText: "Email",
+                                labelStyle: const TextStyle(
+                                    color: AppColors.greyAccent, fontSize: 15),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    borderSide: const BorderSide(
+                                        color: AppColors.primaryColor)),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                    color: AppColors.greyAccent,
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.all(15))),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                              labelText: "Hasło",
+                              labelStyle: const TextStyle(
+                                  color: AppColors.greyAccent, fontSize: 15),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                      color: AppColors.primaryColor)),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide: const BorderSide(
+                                  color: AppColors.greyAccent,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.all(15)),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: "Powtórz hasło",
+                              labelStyle: const TextStyle(
+                                  color: AppColors.greyAccent, fontSize: 15),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  borderSide: const BorderSide(
+                                      color: AppColors.primaryColor)),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide: const BorderSide(
+                                  color: AppColors.greyAccent,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.all(15)),
+                          obscureText: true,
+                        ),
+                        Container(
+                          width: 400,
+                          margin: const EdgeInsets.only(top: 20),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.primaryColor,
+                              textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700),
+                              padding: const EdgeInsets.all(13),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                            ),
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                postData(); 
+                              }
+                            },
+                            child: const Text(
+                              'Zarejestruj się',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
